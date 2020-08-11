d627165712e2e1917f3d32eeea1340fc05aded63
donutwizard666 <thebeardedwarrior@icloud.com>
2020-08-11 07:58:13 -0700

    added some tests

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -10,6 +10,8 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/');
     // checking that the url matches
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     // checking that h2 has the text
     assert.dom('h2').hasText('Welcome to Super Rentals');
     // the rest is pretty self explanatory
@@ -24,6 +26,8 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/about');
     
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals');
   
     assert.dom('.jumbo a.button').hasText('Contact Us');
@@ -37,6 +41,8 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals')
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('a.button').hasText('About');
@@ -44,5 +50,21 @@ module('Acceptance | super rentals', function(hooks) {
 
     assert.equal(currentURL(), '/about');
   })
+
+  test('navigating using the nav bar', async function(assert) {
+    await visit('/')
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.equal(currentURL(), '/');
+  });
 });
 
